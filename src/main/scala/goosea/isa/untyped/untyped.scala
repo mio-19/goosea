58780f0999a63e8c55b35897e9de35ec8cac4880
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-03-13 11:57:49 +0000

    up

--- src/main/scala/goosea/isa/untyped/untyped.scala
+++ src/main/scala/goosea/isa/untyped/untyped.scala
@@ -5,8 +5,14 @@ import scodec.bits._
 import scodec.codecs._
 
 // TODO
-case class RType(opcode: Int, rd: Int, funct3: Int, rs1: Int, rs2: Int, funct7: Int)
-implicit val rtypeCodec: Codec[RType] = (("opcode" | uint(7)) :: ("rd" | uint(5)) :: ("funct3" | uint(3)) :: ("rs1" | uint(5))::("rs2"|uint(5)) :: ("funct7" | uint(5))).as[RType]
+final case class RType(opcode: Int, rd: Int, funct3: Int, rs1: Int, rs2: Int, funct7: Int)
 
-case class IType(opcode: Int, rd: Int, funct3: Int, rs1: Int, imm11_0: Int)
-implicit val itypeCodec: Codec[IType] = (("opcode" | uint(7)) :: ("rd" | uint(5))::("funct3"|uint(3))::("rs1"|uint(5))::("imm11_0"|uint(12))).as[IType]
+implicit val rtypeCodec: Codec[RType] = (("opcode" | uint(7)) :: ("rd" | uint(5)) :: ("funct3" | uint(3)) :: ("rs1" | uint(5)) :: ("rs2" | uint(5)) :: ("funct7" | uint(5))).as[RType]
+
+final case class IType(opcode: Int, rd: Int, funct3: Int, rs1: Int, imm11_0: Int)
+
+implicit val itypeCodec: Codec[IType] = (("opcode" | uint(7)) :: ("rd" | uint(5)) :: ("funct3" | uint(3)) :: ("rs1" | uint(5)) :: ("imm11_0" | uint(12))).as[IType]
+
+final case class SType(opcode: Int, imm4_0: Int, funct3: Int, rs1: Int, rs2: Int, imm11_5: Int)
+
+implicit val stypeCodec: Codec[SType] = (("opcode" | uint(7)) :: ("imm4_0" | uint(5)) :: ("funct3" | uint(3)) :: ("rs1" | uint(5)) :: ("rs2" | uint(5)) :: ("imm11_5" | uint(7))).as[SType]
