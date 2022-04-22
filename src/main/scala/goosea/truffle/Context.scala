2f585fc67120d086b7d0f86aba2fa6314c79ec20
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-04-22 11:26:39 +0000

    up

--- src/main/scala/goosea/truffle/Context.scala
+++ src/main/scala/goosea/truffle/Context.scala
@@ -1,14 +1,14 @@
 package goosea.truffle
 
 import goosea.cpu.Regs
-import goosea.utils.U64
+import goosea.utils._
 import goosea.mem._
 
-final case class Context(regs: Regs, mem: Mem) {
+final case class Context(regs: Regs, mem: Mem, jit: ConcurrentCache[U64, GooseaNode]) {
 }
 
 object Context {
-  def apply(pc: U64): Context = Context(Regs(pc), Mem())
+  def apply(pc: U64): Context = Context(Regs(pc), Mem(), ConcurrentCache())
 
-  def apply(): Context = Context(Regs(), Mem())
+  def apply(): Context = Context(Regs(), Mem(), ConcurrentCache())
 }
---
+++ src/main/scala/goosea/utils/ConcurrentCache.scala
@@ -0,0 +1,25 @@
+package goosea.utils
+
+import scala.ref.WeakReference
+import scala.collection.concurrent.TrieMap
+
+final class ConcurrentCache[K, V <: AnyRef] {
+  private val cache = TrieMap[K, WeakReference[V]]()
+
+  def get(key: K): Option[V] = cache.get(key) match {
+    case Some(ref) => ref.get match {
+      case Some(v) => Some(v)
+      case None => {
+        cache.remove(key, ref)
+        None
+      }
+    }
+    case None => None
+  }
+
+  def put(key: K, value: V): Unit = cache.put(key, WeakReference(value))
+}
+
+object ConcurrentCache {
+  def apply[K, V <: AnyRef](): ConcurrentCache[K, V] = new ConcurrentCache[K, V]()
+}
