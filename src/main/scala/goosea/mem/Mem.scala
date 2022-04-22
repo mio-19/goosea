f5d06c2582c05f053440993eadbd097f5cc9cffc
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-04-22 10:40:14 +0000

    up

--- src/main/scala/goosea/mem/Mem.scala
+++ src/main/scala/goosea/mem/Mem.scala
@@ -8,21 +8,29 @@ val pageSize: Int = 1024 * 1024 // 2^20
 val pageAddr: Int = 20
 val pageMask: Int = (1 << pageAddr) - 1
 
-// TODO
+// 1GB
+// thread-safe
 final class Mem(pageNumber: Int = 1024) {
   val buff: Array[ByteBuffer] = new Array[ByteBuffer](pageNumber)
 
   def get(addr: Int): Byte = {
     val page = buff(addr >> pageAddr)
-    if (page == null) 0 else page.get(addr & pageMask)
+    if (page == null) 0 else page.synchronized { page.get(addr & pageMask) }
   }
 
   def put(addr: Int, x: Byte) = {
     var page = buff(addr >> pageAddr)
     if (page == null) {
-      page = ByteBuffer.allocateDirect(pageSize)
-      buff(addr >> pageAddr) = page
+      buff.synchronized {
+        page = buff(addr >> pageAddr)
+        if (page == null) {
+          page = ByteBuffer.allocate(pageSize)
+          buff(addr >> pageAddr) = page
+        }
+      }
     }
-    page.put(addr & pageMask, x)
+    page.synchronized {
+      page.put(addr & pageMask, x)
+    }
   }
 }
